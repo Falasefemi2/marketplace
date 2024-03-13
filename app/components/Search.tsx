61eda1602627a896b730a933e58060c3605214ad
Falasefemi2 <femifalase228@gmail.com>
2024-03-13 20:15:11 +0100

    feat(navbar)

---
+++ app/components/Search.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Input } from "@/components/ui/input"
+
+
+const Search = () => {
+    return (
+
+
+        <Input />
+
+    );
+}
+
+export default Search;
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const UserMenu = () => {
+    return (
+        <div>
+            user
+        </div>
+    );
+}
+
+export default UserMenu;
