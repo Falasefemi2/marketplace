60b7ee89c9ca48d4311485370a95eebb6e095e8e
Falasefemi2 <femifalase228@gmail.com>
2024-03-13 21:01:06 +0100

    feat(modals)

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,33 @@
 "use client";
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+
 const UserMenu = () => {
     return (
-        <div>
-            user
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => { }}>Login</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => { }}>Sign up</DropdownMenuItem>
+            </DropdownMenuContent>
+
+        </DropdownMenu>
     );
 }
 
